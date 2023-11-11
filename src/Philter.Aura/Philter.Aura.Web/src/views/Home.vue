a746cc44a8ae131d99ed70a4741a4f249764252e
kelseymcmahon <kelsey.mcmahon@hotmail.com>
2023-11-11 12:05:00 -0800

    edits

--- src/Philter.Aura/Philter.Aura.Web/src/views/Home.vue
+++ src/Philter.Aura/Philter.Aura.Web/src/views/Home.vue
@@ -1,24 +1,26 @@
 <template>
   <v-container>
     <c-loader-status :loaders="{ '': [houseList.$load] }" :progressPlaceholder="false">
-      <div v-if="house">
-        <CardWithIcon :title="house.name!" icon="fas fa-house" color="purple">
-          <div>Address: {{ house.address }}</div>
-          <div>Main Phone: {{ house.mainPhone }}</div>
-          <div>Alternate Phone: {{ house.altPhone }}</div>
-        </CardWithIcon>
-      </div>
-      <div v-else>
-        <v-row>
-          <v-col cols="8">
-            <CardWithIcon title="Create A Home" icon="fas fa-house" color="grey">
-              <template #default>
-                <EditHouseForm />
-              </template>
-            </CardWithIcon>
-          </v-col>
-        </v-row>
-      </div>
+      <v-row>
+        <v-col cols="6" v-if="house">
+          <CardWithIcon :title="house.name!" icon="fas fa-house" color="purple">
+            <div>Address: {{ house.address }}</div>
+            <div>Main Phone: {{ house.mainPhone }}</div>
+            <div>Alternate Phone: {{ house.altPhone }}</div>
+          </CardWithIcon>
+        </v-col>
+        <v-col cols="8" v-else>
+          <v-row>
+            <v-col cols="6">
+              <CardWithIcon title="Create A Home" icon="fas fa-house" color="grey">
+                <template #default>
+                  <EditHouseForm />
+                </template>
+              </CardWithIcon>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
     </c-loader-status>
   </v-container>
 </template>
@@ -32,6 +34,6 @@ const houseList = new HouseListViewModel();
 houseList.$pageSize = 100;
 houseList.$load();
 
-const house = houseList?.$items[0];
+const house = computed(() => houseList?.$items[0]);
 
 </script>
