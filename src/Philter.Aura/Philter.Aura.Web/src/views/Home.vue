9c639b6a67cda3d0d13f69d26f72eaa39f238719
Kelly Adams <kelly@intellitect.com>
2023-11-11 14:37:20 -0800

    Fix some linter whining

--- src/Philter.Aura/Philter.Aura.Web/src/views/Home.vue
+++ src/Philter.Aura/Philter.Aura.Web/src/views/Home.vue
@@ -25,9 +25,11 @@
           <v-btn color="purple" density="comfortable" icon="fas fa-plus" variant="tonal" />
         </v-col>
         <v-col cols="12"><v-divider /></v-col>
-        <v-col cols="12" md="6" lg="4" xl="3" v-for="room in house.rooms" v-if="house.rooms">
-          <RoomCard :room="room" color="purple" />
-        </v-col>
+        <span v-if="house.rooms">
+          <v-col cols="12" md="6" lg="4" xl="3" v-for="room in house.rooms" v-bind:key="room.roomId">
+            <RoomCard :room="room" color="purple" />
+          </v-col>
+        </span>
         <v-col cols="12" class="font-italic text-medium-emphasis" v-else>
           No rooms
         </v-col>
@@ -37,19 +39,19 @@
 </template>
 
 <script setup lang="ts">
-import { HouseListViewModel } from "@/viewmodels.g";
+  import { HouseListViewModel } from "@/viewmodels.g";
 
-useTitle("Home");
+  useTitle("Home");
 
-const houseList = ref<HouseListViewModel>();
-houseList.value = new HouseListViewModel();
-houseList.value.$pageSize = 100;
-houseList.value.$load();
+  const houseList = ref<HouseListViewModel>();
+  houseList.value = new HouseListViewModel();
+  houseList.value.$pageSize = 100;
+  houseList.value.$load();
 
-const house = computed(() => houseList.value?.$items[0]);
+  const house = computed(() => houseList.value?.$items[0]);
 
-function loadHouses() {
-  houseList.value?.$load();
-}
+  function loadHouses() {
+    houseList.value?.$load();
+  }
 
 </script>
