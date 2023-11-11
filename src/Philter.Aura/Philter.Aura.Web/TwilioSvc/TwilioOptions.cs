7327ce48df88704f2f2a93add2de55bca356b71a
Josh Campbell <Josh.Campbell@IntelliTect.com>
2023-11-11 14:23:05 -0800

    init

--- src/Philter.Aura/Philter.Aura.Web/TwilioSvc/TwilioOptions.cs
+++ src/Philter.Aura/Philter.Aura.Web/TwilioSvc/TwilioOptions.cs
@@ -2,6 +2,6 @@
 
 public class TwilioOptions
 {
-    public string AccountSid { get; set; }
-    public string AuthToken { get; set; }
+    public string AccountSid { get; set; } = "ACd1cc6523daf963fd0473cd42d40e1233";
+    public string AuthToken { get; set; } = "5a07d6131f514557944f1c0707105a39";
 }
