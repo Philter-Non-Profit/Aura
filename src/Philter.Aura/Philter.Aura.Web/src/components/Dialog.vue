ce694e1ba855b55c39125c77b4af952f9f2dd3ea
Kelsey McMahon <40438334+kelseymcmahon@users.noreply.github.com>
2023-11-11 14:17:08 -0800

    feat: UI add ability to create new house and edit house (#22)

---
+++ src/Philter.Aura/Philter.Aura.Web/src/components/Dialog.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-dialog max-width="800px" scrollable>
+        <v-card>
+            <v-card-item class="bg-primary pt-4 pb-3">
+                <v-row>
+                    <v-col cols="12" class="my-auto">
+                        <v-card-title> {{ title }} </v-card-title>
+                    </v-col>
+                </v-row>
+            </v-card-item>
+            <v-card-text class="py-4">
+                <slot />
+            </v-card-text>
+        </v-card>
+    </v-dialog>
+</template>
+  
+<script setup lang="ts">
+defineProps<{
+    title: string;
+}>();
+</script>
---
+++ src/Philter.Aura/Philter.Aura.Web/src/components/HouseCard.vue
@@ -0,0 +1,46 @@
+<template>
+    <CardWithIcon :title="house.name!" icon="fas fa-house" :color="color">
+        <template #button>
+            <v-btn :color="color" density="comfortable" icon="fas fa-pencil" variant="tonal"
+                @click="editHouse = !editHouse" />
+        </template>
+        <div class="pb-3">
+            <v-icon color="purple" icon="fas fa-location-dot" start />
+            <span class="font-weight-bold">Address:</span>
+            {{ house.address }}
+        </div>
+        <div class="pb-3">
+            <v-icon color="purple" icon="fas fa-mobile-screen-button" start />
+            <span class="font-weight-bold">Main Phone:</span>
+            {{ house.mainPhone }}
+        </div>
+        <div class="pb-3">
+            <v-icon color="purple" icon="fas fa-mobile-screen-button" start />
+            <span class="font-weight-bold">Alternate Phone:</span>
+            {{ house.altPhone }}
+        </div>
+    </CardWithIcon>
+
+    <Dialog title="Edit" v-model="editHouse">
+        <EditHouseForm :house="house" @saved="editHouse = false">
+            <template #buttons>
+                <v-btn color="primary" variant="tonal" class="mr-3" @click="editHouse = false">Cancel</v-btn>
+            </template>
+        </EditHouseForm>
+    </Dialog>
+</template>
+
+<script lang="ts" setup>
+import { HouseViewModel } from "@/viewmodels.g";
+withDefaults(
+    defineProps<{
+        house: HouseViewModel;
+        color?: string;
+    }>(),
+    {
+        color: "primary",
+    }
+);
+
+const editHouse = ref(false);
+</script>
