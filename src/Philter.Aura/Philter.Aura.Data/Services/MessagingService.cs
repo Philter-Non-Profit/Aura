1a5aba9a1501f9a70803e0c65d74a1b732c628ca
TylerRose <tylerrose25565@gmail.com>
2023-11-11 13:17:12 -0800

    Add restriction to how far out messages can be scheduled per twilio's api

--- src/Philter.Aura/Philter.Aura.Data/Services/MessagingService.cs
+++ src/Philter.Aura/Philter.Aura.Data/Services/MessagingService.cs
@@ -1,7 +1,9 @@
 using IntelliTect.Coalesce.Models;
 using System;
 using System.Threading.Tasks;
+using Twilio;
 using Twilio.Rest.Api.V2010.Account;
+using Twilio.Rest.Proxy.V1.Service.Session.Participant;
 using Twilio.Types;
 
 namespace Philter.Aura.Data.Services;
@@ -24,6 +26,10 @@ public class MessagingService : IMessagingService
 
     public async Task<ItemResult<MessageResource>> SendTextAt(PhoneNumber to, string messagingServiceId, string message, DateTime messageTime)
     {
+        if(DateTime.Now.AddDays(7) < messageTime)
+        {
+            return "You cannot schedule a message further than 7 days in advance.";
+        }
         var result = await MessageResource.CreateAsync(to: to, body: message,
             messagingServiceSid: messagingServiceId, sendAt: messageTime);
 
