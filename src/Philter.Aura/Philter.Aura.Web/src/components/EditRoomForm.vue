11c671446db483ac22d7b9c7000c1377c5b4715f
kelseymcmahon <kelsey.mcmahon@hotmail.com>
2024-05-14 21:47:36 -0700

    edits

---
+++ src/Philter.Aura/Philter.Aura.Web/src/components/EditRoomForm.vue
@@ -0,0 +1,51 @@
+<template>
+    <c-loader-status :loaders="{ '': [editRoom!.$save] }" />
+    <v-form ref="form" v-if="editRoom">
+        <v-row>
+            <v-col cols="12">
+                <c-input :model="editRoom" for="name" autofocus></c-input>
+            </v-col>
+            <v-col cols="12">
+                <c-input :model="editRoom" for="notes" textarea min="0"></c-input>
+            </v-col>
+        </v-row>
+    </v-form>
+
+    <v-divider class="my-3" />
+    <div class="d-flex justify-end pt-4">
+        <slot name="buttons" />
+        <v-btn color="primary" variant="elevated" @click="save()"><v-icon icon="fas fa-save" start /> Save </v-btn>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { HouseViewModel, RoomViewModel } from "@/viewmodels.g";
+import { VForm } from "vuetify/components";
+
+const props = defineProps<{
+    room?: RoomViewModel;
+    house: HouseViewModel;
+}>();
+
+const emit = defineEmits<{
+    (e: "saved"): void;
+}>();
+
+const editRoom = computed(() => {
+    let room = new RoomViewModel();
+    room.houseId = props.house.houseId;
+    if (props.room) { room = props.room; }
+    return room;
+});
+
+const form = ref<VForm>();
+
+onMounted(() => {
+    (form.value as VForm)?.validate();
+});
+
+async function save() {
+    await editRoom!.value!.$save();
+    emit("saved");
+}
+</script>
--- src/Philter.Aura/Philter.Aura.Web/src/views/Home.vue
+++ src/Philter.Aura/Philter.Aura.Web/src/views/Home.vue
@@ -1,49 +1,58 @@
 <template>
   <v-container>
     <c-loader-status :loaders="{ '': [houseList?.$load] }">
-      <v-row>
-        <v-col cols="12" md="6" lg="5" xl="4" v-if="house">
-          <HouseCard :house="house" color="purple" />
-        </v-col>
-        <v-col cols="8" v-else>
-          <v-row>
-            <v-col cols="6">
-              <CardWithIcon title="Create A Home" icon="fas fa-house" color="grey">
-                <template #default>
-                  <EditHouseForm @saved="loadHouses()" />
-                </template>
-              </CardWithIcon>
+      <div v-if="houseList">
+        <v-row>
+          <v-col cols="12" md="6" lg="5" xl="4" v-if="house">
+            <HouseCard :house="house" color="purple" />
+          </v-col>
+          <v-col cols="8" v-else>
+            <v-row>
+              <v-col cols="6">
+                <CardWithIcon title="Create A Home" icon="fas fa-house" color="grey">
+                  <template #default>
+                    <EditHouseForm @saved="loadHouses()" />
+                  </template>
+                </CardWithIcon>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+        <div v-if="house">
+          <v-row class="mt-4">
+            <v-col cols="12" class="d-flex pb-0">
+              <div class="text-h6 mr-2">
+                Rooms
+              </div>
+              <v-btn @click="editRoom = !editRoom" color="purple" density="comfortable" icon="fas fa-plus" variant="tonal" />
+            </v-col>
+            <v-col cols="12"><v-divider /></v-col>
+          </v-row>
+          <v-row v-if="house.rooms">
+            <v-col cols="12" md="6" lg="4" xl="3" v-for="room in house.rooms" v-bind:key="room.roomId!">
+              <RoomCard :room="room" color="purple" />
             </v-col>
           </v-row>
-        </v-col>
-      </v-row>
-      <div v-if="house">
-        <v-row class="mt-4">
-          <v-col cols="12" class="d-flex pb-0">
-            <div class="text-h6 mr-2">
-              Rooms
-            </div>
-            <v-btn color="purple" density="comfortable" icon="fas fa-plus" variant="tonal" />
-          </v-col>
-          <v-col cols="12"><v-divider /></v-col>
-        </v-row>
-        <v-row v-if="house.rooms">
-          <v-col cols="12" md="6" lg="4" xl="3" v-for="room in house.rooms" v-bind:key="room.roomId!">
-            <RoomCard :room="room" color="purple" />
-          </v-col>
-        </v-row>
-        <v-row v-else>
-          <v-col cols="12" class="font-italic text-medium-emphasis">
-            No rooms
-          </v-col>
-        </v-row>
+          <v-row v-else>
+            <v-col cols="12" class="font-italic text-medium-emphasis">
+              No rooms
+            </v-col>
+          </v-row>
+        </div>
       </div>
+      <Dialog title="Edit" v-model="editRoom">
+          <EditRoomForm :room="room" :house="house!" @saved="loadHouses(); editRoom = false">
+              <template #buttons>
+                  <v-btn color="primary" variant="tonal" class="mr-3" @click="editRoom = false">Cancel</v-btn>
+              </template>
+          </EditRoomForm>
+      </Dialog>
     </c-loader-status>
   </v-container>
 </template>
 
 <script setup lang="ts">
-  import { HouseListViewModel } from "@/viewmodels.g";
+  import { HouseListViewModel, RoomViewModel } from "@/viewmodels.g";
 
   useTitle("Home");
 
@@ -53,6 +62,8 @@
   houseList.value.$load();
 
   const house = computed(() => houseList.value?.$items[0]);
+  const room: Ref<undefined | RoomViewModel> = ref(undefined);
+  const editRoom = ref(false);
 
   function loadHouses() {
     houseList.value?.$load();
