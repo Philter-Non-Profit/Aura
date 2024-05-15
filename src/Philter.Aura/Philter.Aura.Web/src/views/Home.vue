3a8b878cb45ed00e70074839abca590668a85110
kelseymcmahon <kelsey.mcmahon@hotmail.com>
2024-05-14 20:40:45 -0700

    edits

--- src/Philter.Aura/Philter.Aura.Web/src/views/Home.vue
+++ src/Philter.Aura/Philter.Aura.Web/src/views/Home.vue
@@ -17,23 +17,27 @@
           </v-row>
         </v-col>
       </v-row>
-      <v-row class="mt-4" v-if="house">
-        <v-col cols="12" class="d-flex pb-0">
-          <div class="text-h6 mr-2">
-            Rooms
-          </div>
-          <v-btn color="purple" density="comfortable" icon="fas fa-plus" variant="tonal" />
-        </v-col>
-        <v-col cols="12"><v-divider /></v-col>
-        <span v-if="house.rooms">
+      <div v-if="house">
+        <v-row class="mt-4">
+          <v-col cols="12" class="d-flex pb-0">
+            <div class="text-h6 mr-2">
+              Rooms
+            </div>
+            <v-btn color="purple" density="comfortable" icon="fas fa-plus" variant="tonal" />
+          </v-col>
+          <v-col cols="12"><v-divider /></v-col>
+        </v-row>
+        <v-row v-if="house.rooms">
           <v-col cols="12" md="6" lg="4" xl="3" v-for="room in house.rooms" v-bind:key="room.roomId!">
             <RoomCard :room="room" color="purple" />
           </v-col>
-        </span>
-        <v-col cols="12" class="font-italic text-medium-emphasis" v-else>
-          No rooms
-        </v-col>
-      </v-row>
+        </v-row>
+        <v-row v-else>
+          <v-col cols="12" class="font-italic text-medium-emphasis">
+            No rooms
+          </v-col>
+        </v-row>
+      </div>
     </c-loader-status>
   </v-container>
 </template>
